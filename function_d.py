a4dd9b8650c65742ad5b62af69cf1b3cc0eaf6a5
Gloria <gloria.wan.glory@gmail.com>
2023-03-24 13:49:03 -0400

    Gloria version

--- function_d.py
+++ function_d.py
@@ -2,8 +2,12 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    highest_value = 0
 
+    for val in numbers:
+        if val > highest_value:
+            highest_value = val
+        
 
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
